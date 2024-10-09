af8f31566c503790e4a3f4b92ebbacc5322d7ed9
Majkejl <misicek03@gmail.com>
2024-10-09 22:09:48 +0200

    Revert "l"
    This reverts commit ee1d9aff9f4c331ae4d601101da936c85dbbfdac.

--- a-star_visual/render.hpp
+++ a-star_visual/render.hpp
@@ -3,7 +3,7 @@
 #include <SDL.h>
 
 struct Renderer {
-	SDL_Window* gWindow = NULL; 
+	SDL_Window* gWindow = NULL;
 	SDL_Surface* gScreenSurface = NULL;
 	SDL_Surface* gCurrentSurface = NULL;
 	SDL_Renderer* gRenderer = NULL;
