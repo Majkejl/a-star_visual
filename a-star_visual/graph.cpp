dd98eefdc6895c81b5137e9c786706fdcc7a36b4
Majkejl <misicek03@gmail.com>
2024-10-10 00:04:07 +0200

    graph plane constructor

--- a-star_visual/graph.cpp
+++ a-star_visual/graph.cpp
@@ -1,8 +1,12 @@
 #include "graph.hpp"
 
+Graph::Graph(plane_t _p) : plane{ std::make_unique<plane_t>(_p) } 
+{
+	w = plane->data()->size();
+	h = plane->size();
+}
 
-
-Graph::Graph(int _w, int _h) : w(_w), h(_h)
+Graph::Graph(int _w, int _h) : plane{ std::make_unique<plane_t>() }, w{ _w }, h{ _h }
 {
 	for (size_t i = 0; i < h; i++)
 	{
@@ -13,7 +17,7 @@ Graph::Graph(int _w, int _h) : w(_w), h(_h)
 Graph::Graph() : Graph(DEFAULT_SIZE, DEFAULT_SIZE)
 {
 	(*plane)[0][0] = blocks::start;
-	(*plane)[9][9] = blocks::target;
+	(*plane)[DEFAULT_SIZE - 1][DEFAULT_SIZE - 1] = blocks::target;
 }
 
 bool Graph::in_bounds(int x, int y) const
